1bb7b9d1566098e7bc934b780a5bb83145b7b05b
Соль <147088527+uasalt@users.noreply.github.com>
2025-06-02 22:05:38 +0300

    Update efi.lua

--- efi.lua
+++ efi.lua
@@ -26,13 +26,13 @@ end
 while true do
     local gpu = component.proxy(component.list("gpu")())
     local sWidth, sHeight = gpu.getResolution()
+    gpu.fill(0, 0, sWidth, sHeight, " ")
     local event, UUID, a, b, c = computer.pullSignal(1)
     gpu.set(1, 1, "event: " .. tostring(event))
     gpu.set(1, 2, "UUID: " .. tostring(UUID))
     gpu.set(1, 3, "a: " .. tostring(a))
     gpu.set(1, 4, "b: " .. tostring(b))
     gpu.set(1, 5, "c: " .. tostring(c))
-    gpu.fill(0, 0, sWidth, sHeight, " ")
     if ( event == 'key_down' ) then
         if (b == 200 and pos > 0) then -- Стрелка вверх
             pos = pos - 1
