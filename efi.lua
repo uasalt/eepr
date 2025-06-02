dbb8705ff85ca1afd74916742da4b415ec4b4d34
Соль <147088527+uasalt@users.noreply.github.com>
2025-06-02 21:24:57 +0300

    Update efi.lua

--- efi.lua
+++ efi.lua
@@ -1,7 +1,9 @@
 while true do
     local gpu = component.proxy(component.list("gpu")())
     local event, UUID, a, b, c = computer.pullSignal(1)
-    gpu.set(1, 1, tostring(a))
-    gpu.set(1, 2, tostring(b))
-    gpu.set(1, 3, tostring(c))
+    gpu.set(1, 1, "event: " .. tostring(event))
+    gpu.set(1, 2, "UUID: " .. tostring(UUID))
+    gpu.set(1, 3, "a: " .. tostring(a))
+    gpu.set(1, 4, "b: " .. tostring(b))
+    gpu.set(1, 5, "c: " .. tostring(c))
 end
