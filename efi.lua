05b10af9b35e910968bfe5b59bfa1645844e6bb6
Соль <147088527+uasalt@users.noreply.github.com>
2025-06-02 21:18:59 +0300

    Update efi.lua

--- efi.lua
+++ efi.lua
@@ -1,6 +1,7 @@
 while true do
+    local gpu = g
     local event, UUID, a, b, c = computer.pullSignal(1)
-    computer.gpu.set(1, 1, a)
-    computer.gpu.set(1, 2, b)
-    computer.gpu.set(1, 3, c)
+    gpu.set(1, 1, a)
+    gpu.set(1, 2, b)
+    gpu.set(1, 3, c)
 end
