8a18028670d552c2cc2ae95c28cdc383c7f7428e
Соль <147088527+uasalt@users.noreply.github.com>
2025-06-02 22:04:58 +0300

    Update efi.lua

--- efi.lua
+++ efi.lua
@@ -1,3 +1,28 @@
+local menu, pos = {
+    "Disks utility",
+    "Recovery other EEPROM",
+    "Computer info",
+    "Reboot"
+}, 0
+
+function center(text)
+    return math.ceil(sWidth / 2 - (#text /2 )), math.ceil(sHeight / 2) - 1
+end
+
+function drawMenu()
+    gpu.setBackground(0x141414)
+    for i, _ in ipairs(menu) do
+        if (pos == i) then
+            gpu.setBackground(0x202020)
+            local w, h = center(menu[i])
+            gpu.set(w, h - (#menu - pos))
+            gpu.setBackground(0x141414)
+        else
+            gpu.set(center(menu[i]))
+        end
+    end
+end
+
 while true do
     local gpu = component.proxy(component.list("gpu")())
     local sWidth, sHeight = gpu.getResolution()
@@ -8,4 +33,13 @@ while true do
     gpu.set(1, 4, "b: " .. tostring(b))
     gpu.set(1, 5, "c: " .. tostring(c))
     gpu.fill(0, 0, sWidth, sHeight, " ")
+    if ( event == 'key_down' ) then
+        if (b == 200 and pos > 0) then -- Стрелка вверх
+            pos = pos - 1
+            drawMenu()
+        elseif (b == 208 and #menu ~= pos ) then -- Стрелка вниз
+            pos = pos + 1
+            drawMenu()
+        end
+    end
 end
