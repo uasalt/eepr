d5a486d1fea209dc341a7936d01d6abd4f8640d0
Соль <147088527+uasalt@users.noreply.github.com>
2025-06-03 17:04:24 +0300

    Update efi.lua

--- efi.lua
+++ efi.lua
@@ -1,8 +1,20 @@
 local menu, pos, prevPos = {
-    "Disks utility",
-    "Recovery other",
-    "Computer info",
-    "Reboot"
+    {
+        ['title'] = "Disks utility",
+        ['id'] = 0
+    },
+    {
+        ['title'] = "Recovery other",
+        ['id'] = 0
+    },
+    {
+        ['title'] = "Computer info",
+        ['id'] = 0
+    },
+    {
+        ['title'] = "Reboot",
+        ['id'] = 0
+    }
 }, 1, 1
 local general = menu
 
@@ -19,14 +31,14 @@ function drawMenu()
     gpu.setBackground(0x141414)
     gpu.fill(1, 1, sWidth, sHeight, " ")
     for i, item in ipairs(menu) do
-        local w, h = center(item)
+        local w, h = center(item['title'])
         h = h - (#menu // 2) + (i - 1)
         if pos == i then
             gpu.setBackground(0x202020)
         else
             gpu.setBackground(0x141414)
         end
-        gpu.set(w, h + 2, item)
+        gpu.set(w, h + 2, item['title'])
     end
 end
 
@@ -41,6 +53,13 @@ function request(url)
     until not chunk
     handle.close()
     return data
+end
+
+function contains(tbl, val)
+  for _, v in ipairs(tbl) do
+    if v == val then return true end
+  end
+  return false
 end
 
 while true do
@@ -59,59 +78,120 @@ while true do
             pos = pos + 1
             drawMenu()
         elseif ( b == 28 ) then -- Enter
-            if ( menu[pos] == 'Reboot' ) then
+            if ( menu[pos]['title'] == 'Reboot' ) then
                 computer.shutdown(true)
-            elseif ( menu[pos] == 'Disks utility' ) then
-                menu = { 'back' }
+            elseif ( menu[pos]['title'] == 'Disks utility' ) then
+                menu = { {
+                        ['title'] = 'back',
+                        ['id'] = 0
+                } }
                 for address in component.list('filesystem') do
                     local fs = component.proxy(address)
-                    table.insert(menu, (fs.getLabel() or address) .. " [HDD]" .. (fs.isReadOnly() and "[RO]" or "[RW]") .. (eeprom.getData() == address and "[BOOT]" or ""))
+                    table.insert(menu, {
+                        ['title'] = (fs.getLabel() or address) .. " [HDD]" .. (fs.isReadOnly() and "[RO]" or "[RW]") .. (eeprom.getData() == address and "[BOOT]" or ""),
+                        ['id'] = address
+                    })
                 end
                 for address in component.list('disk_drive') do
                     local floppy = component.proxy(address)
                     if not floppy.isEmpty() then
                         local fs = component.proxy(floppy['media'][0])
-                        table.insert(menu, (fs.getLabel() or address) .. " [HDD]" .. (fs.isReadOnly() and "[RO]" or "[RW]") .. (eeprom.getData() == address and "[BOOT]" or ""))
+                        table.insert(menu, {
+                            ['title'] = (fs.getLabel() or address) .. " [HDD]" .. (fs.isReadOnly() and "[RO]" or "[RW]") .. (eeprom.getData() == address and "[BOOT]" or ""),
+                            ['id'] = address
+                        })
                     end
                 end
                 prevPos = menu[pos]
                 pos = 1
                 drawMenu()
-            elseif ( menu[pos] == 'Recovery other' ) then
+            elseif ( menu[pos]['id'] ~= 0 and #menu[pos]['id'] == 36 and not contains({"Set as bootable", "Change label", "Erase", "back"}, menu[pos]['title']) ) then
                 menu = {
-                    "uEFI",
-                    "MineOS EFI",
-                    "BIOS",
-                    "back"
+                    {
+                        ['title'] = "Set as bootable",
+                        ['id'] = menu[pos]['id']
+                    },
+                    {
+                        ['title'] = "Change label",
+                        ['id'] = menu[pos]['id']
+                    },
+                    {
+                        ['title'] = "Erase",
+                        ['id'] = menu[pos]['id']
+                    },
+                    {
+                        ['title'] = "back",
+                        ['id'] = menu[pos]['id']
+                    }
                 }
                 prevPos = menu[pos]
                 pos = 1
                 drawMenu()
-            elseif (menu[pos] == 'uEFI') then
+            elseif contains({"Set as bootable", "Change label", "Erase", "back"}, menu[pos]['title']) then
+                if menu[pos]['title'] == 'Set as bootable' then
+                    eeprom.setData(menu[pos]['id'])
+                elseif menu[pos]['title'] == 'Erase' then
+                    component.proxy(menu[pos]['id']).format()
+                    pos = 1
+                    menu = general
+                    drawMenu()
+                end
+            elseif ( menu[pos]['title'] == 'Recovery other' ) then
+                menu = {
+                    {
+                        ['title'] = "uEFI",
+                        ['id'] = 0
+                    },
+                    {
+                        ['title'] = "MineOS EFI",
+                        ['id'] = 0
+                    },
+                    {
+                        ['title'] = "BIOS",
+                        ['id'] = 0
+                    },
+                    {
+                        ['title'] = "back",
+                        ['id'] = 0
+                    }
+                }
+                prevPos = menu[pos]
+                pos = 1
+                drawMenu()
+            elseif (menu[pos]['title'] == 'uEFI') then
                 local code = request("https://raw.githubusercontent.com/uasalt/eepr/refs/heads/main/installer.lua")
                 eeprom.set(code)
                 eeprom.setLabel("uEFI")
-            elseif ( menu[pos] == 'MineOS EFI' ) then
+            elseif ( menu[pos]['title'] == 'MineOS EFI' ) then
                 menu = {
-                    "EFI",
-                    "Installer",
-                    "back"
+                    {
+                        ["title"] = "EFI",
+                        ["id"] = 0
+                    },
+                    {
+                        ["title"] = "Installer",
+                        ["id"] = 0
+                    },
+                    {
+                        ["title"] = "back",
+                        ["id"] = 0
+                    }
                 }
                 prevPos = menu[pos]
                 pos = 1
                 drawMenu()
-            elseif ( menu[pos] == 'EFI' and prevPos == 'MineOS EFI' ) then
+            elseif ( menu[pos]['title'] == 'EFI' and prevPos['title'] == 'MineOS EFI' ) then
                 local code = request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/refs/heads/master/EFI/Minified.lua")
                 eeprom.set(code)
                 eeprom.setLabel("MineOS EFI")
-            elseif ( menu[pos] == 'Installer' and prevPos == 'MineOS EFI' ) then
+            elseif ( menu[pos]['title'] == 'Installer' and prevPos['title'] == 'MineOS EFI' ) then
                 eeprom.setLabel("MineOS EFI")
                 eeprom.set([[local connection, data, chunk = component.proxy(component.list("internet")()).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua"), "";while true do chunk = connection.read(math.huge);if chunk then data = data .. chunk else break end;end;connection.close();load(data)()]])
-            elseif ( menu[pos] == 'BIOS' ) then
+            elseif ( menu[pos]['title'] == 'BIOS' ) then
                 local code = request("https://raw.githubusercontent.com/uasalt/eepr/refs/heads/main/BIOS.lua")
                 eeprom.setLabel("EEPROM (Lua BIOS)")
                 eeprom.set(code)
-            elseif ( menu[pos] == 'back' ) then
+            elseif ( menu[pos]['title'] == 'back' ) then
                 menu = general
                 prevPos = menu[pos]
                 pos = 1
